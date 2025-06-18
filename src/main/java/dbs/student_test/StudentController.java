61b6b464680405c5e3995883a3c285198ed77446
dancrump1 <danielcrump@outlook.com>
2025-06-18 13:14:42 -0400

    add reusable fragment and homepage with form tied to students page

--- src/main/java/dbs/student_test/StudentController.java
+++ src/main/java/dbs/student_test/StudentController.java
@@ -5,17 +5,22 @@ import dbs.student_test.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
+@RequestMapping("/students")
 public class StudentController {
 
     @GetMapping
-    String getStudent(Model model) {
+    String getStudent(Model model, @RequestParam(required = false, name = "name") String name) {
         model.addAttribute("something", "this is something");
-        model.addAttribute("student", Arrays.asList(new Student("john", "semor", "john@semor"), new Student("jim", "bob", "jim@bob")));
+        model.addAttribute("student", List.of(new Student("john", name, "john@semor")));
 
+//        Return same name as html file
         return "student";
     }
 }
