2dc1f0803ef52088bfc4fc5769be578e697f5dff
dancrump1 <danielcrump@outlook.com>
2025-09-03 14:29:22 -0400

    add auto-gen security

--- src/main/java/dbs/student_test/security/SecurityConfig.java
+++ src/main/java/dbs/student_test/security/SecurityConfig.java
@@ -5,26 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder().username("john").password("{noop}test123")
-                .roles("EMPLOYEE").build();
-        UserDetails suzy = User.builder().username("suzy").password("{noop}test456")
-                .roles("EMPLOYEE", "MANAGER").build();
-        UserDetails jane = User.builder().username("jane").password("{noop}test789")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
-        return new InMemoryUserDetailsManager(john, suzy, jane);
+    //    add support for JDBC and no hardcoded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager((dataSource));
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -38,4 +35,16 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    //    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder().username("john").password("{noop}test123")
+//                .roles("EMPLOYEE").build();
+//        UserDetails suzy = User.builder().username("suzy").password("{noop}test456")
+//                .roles("EMPLOYEE", "MANAGER").build();
+//        UserDetails jane = User.builder().username("jane").password("{noop}test789")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(john, suzy, jane);
+//    }
 }
