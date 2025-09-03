0c3d4a0f057bfb2b9f596523f13ccb530f9765d7
dancrump1 <danielcrump@outlook.com>
2025-09-03 13:26:28 -0400

    security on top of routes

---
+++ src/main/java/dbs/student_test/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package dbs.student_test.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder().username("john").password("{noop}test123")
+                .roles("EMPLOYEE").build();
+        UserDetails suzy = User.builder().username("suzy").password("{noop}test456")
+                .roles("EMPLOYEE", "MANAGER").build();
+        UserDetails jane = User.builder().username("jane").password("{noop}test789")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(john, suzy, jane);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer.requestMatchers(HttpMethod.GET, "/magic-api/categories").hasRole("MANAGER")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
