6e04266e77408bd0cfb93e092ecac7188d22c50d
dancrump1 <danielcrump@outlook.com>
2025-09-25 16:19:07 -0400

    Add application architechture steps

--- src/main/java/dbs/student_test/DemoApplication.java
+++ src/main/java/dbs/student_test/DemoApplication.java
@@ -17,4 +17,11 @@ public class DemoApplication {
 //
         };
     }
+
+//    Basic application usage
+//    1. fetch categories and the components contained within
+//    2. If no categories, show field on frontend to create first category
+//    3. If category contains no component, show list of all components and ability to send selected copmoennt to save as a compoennt within the category
+//    4. Allow editing title and description of categories
+//    5. Allow editing the description of the components
 }
